167bdd34803b00d7dae7368e04990848873ff380
weijia18 <2658402573@qq.com>
2020-09-29 17:32:55 +0800

    update

---
+++ conf/database.js
@@ -0,0 +1,10 @@
+
+const mysql = {
+    host: 'http://106.53.51.117/',
+    user: 'root',
+    password: 'mztkn12345',
+    database: 'myblog',
+    multipleStatements: true
+}
+
+module.exports=mysql
